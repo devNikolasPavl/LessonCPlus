b43b1a843710a54577228c929a917426fb939f59
Nikolas Pavlov <devnikolaspavl@gmail.com>
2022-12-22 17:17:37 +0300

    Book_01, lesson_12

---
+++ ru.book_01.lesson_12/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Nikolay Pavlov on 22.12.2022.
+//
+
+#include <iostream>
+
+int main()
+{
+    // это однострочный комментарий
+
+    // cout и endl находятся в библиотеке iostream
+    std::cout << "Hello, world" << std::endl;
+
+    /*
+     * многострочный комментарий
+     */
+    std::cout << "It is so exciting!" << std::endl;
+    return 0;
+}
